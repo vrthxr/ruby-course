5a6b0c61e63c4323c4a38cca776873fa357c425e
Arthur C <constancio_arthur@yahoo.com>
2026-06-02 17:12:40 -0300

    style: fix indentation and spacing in game_spec

--- aula-09-rspec/spec/game_spec.rb
+++ aula-09-rspec/spec/game_spec.rb
@@ -12,24 +12,45 @@ RSpec.describe 'Game' do
   before(:each) do
     @game = Game.new('Spider-Man: Miles Morales', 2020, ['Open-World', 'Action'])
   end
+
   context 'when dealing with PS5 games' do
     it 'should have a title' do
-      # expect(@game.title).to eq('Spider-Man: Miles Morales')
-      expect(@game.title).to be_truthy #verifica se uma condição é true ou false
+      expect(@game.title).to be_truthy
     end
+
     it 'should allow setting and updating the title' do
       @game.title = 'Demon Souls'
       expect(@game.title).to eq('Demon Souls')
       @game.title = 'Ratchet & Clank Rift Apart'
       expect(@game.title).to eq('Ratchet & Clank Rift Apart')
     end
+
     it 'should have a release year within a specific range' do
-      #verifica se um valor está dentro de um intervalo specifico
       expect(@game.release_year).to be_within(1).of(2020)
     end
+
     it 'should include specific features' do
-    expect(@game.features).to include('Open-World')
-    expect(@game.features).to include('Action')
+      expect(@game.features).to include('Open-World')
+      expect(@game.features).to include('Action')
+    end
+  end
+
+  context 'role-playing-games (RPGs)' do
+    before(:each) do
+      @rpg_game = Game.new('Demon Souls', 2020, ['RPG', 'Action'])
+    end
+
+    it 'should have a title' do
+      expect(@rpg_game.title).to be_truthy
+    end
+
+    it 'should have a release year within a specific range' do
+      expect(@rpg_game.release_year).to be_within(1).of(2020)
+    end
+
+    it 'should include specific features' do
+      expect(@rpg_game.features).to include('RPG')
+      expect(@rpg_game.features).to include('Action')
     end
   end
 end
