3d9ecbee3b62d07d13cf1bee933c5e87b4fbac64
Arthur C <constancio_arthur@yahoo.com>
2026-06-02 16:40:11 -0300

    fix: use positional args in Game initialize and update spec accordingly

--- aula-09-rspec/lib/models/game.rb
+++ aula-09-rspec/lib/models/game.rb
@@ -1,6 +1,8 @@
 class Game
-  attr_accessor :title
-    def initialize (title)
+  attr_accessor :title, :release_year, :features
+    def initialize (title, release_year= nil, features= [])
         @title = title
+        @release_year = release_year
+        @features = features
     end
 end
--- aula-09-rspec/spec/game_spec.rb
+++ aula-09-rspec/spec/game_spec.rb
@@ -10,18 +10,22 @@ require "models/game"
 
 RSpec.describe 'Game' do
   before(:each) do
-    @game = Game.new('Spider-Man: Miles Morales')
+    @game = Game.new('Spider-Man: Miles Morales', 2020, ['Open-World', 'Action'])
   end
   context 'when dealing with PS5 games' do
     it 'should have a title' do
-      expect(@game.title).to eq('Spider-Man: Miles Morales')
+      # expect(@game.title).to eq('Spider-Man: Miles Morales')
+      expect(@game.title).to be_truthy #verifica se uma condição é true ou false
     end
     it 'should allow setting and updating the title' do
       @game.title = 'Demon Souls'
       expect(@game.title).to eq('Demon Souls')
       @game.title = 'Ratchet & Clank Rift Apart'
       expect(@game.title).to eq('Ratchet & Clank Rift Apart')
-
+    end
+    it 'should have a release year within a specific range' do
+      #verifica se um valor está dentro de um intervalo specifico
+      expect(@game.release_year).to be_within(1).of(2020)
     end
   end
 end
