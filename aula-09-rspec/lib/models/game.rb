41f61928190837c11b92962c903b04be463fc425
Arthur C <constancio_arthur@yahoo.com>
2026-06-02 16:29:25 -0300

    feat: add before hook, extract Game model and expand game specs

---
+++ aula-09-rspec/lib/models/game.rb
@@ -0,0 +1,6 @@
+class Game
+  attr_accessor :title
+    def initialize (title)
+        @title = title
+    end
+end
--- aula-09-rspec/spec/game_spec.rb
+++ aula-09-rspec/spec/game_spec.rb
@@ -1,24 +1,27 @@
+# frozen_string_literal: true
+
 # Describe - Usado para descrever um grupo de exemplos de teste
 # It - Usado para definir um exemplo de teste específico
 # Expect - iniciar uma expectativa sobre um determinado objeto ou valor.
-# Eq -  usado para verificar se dois valores são iguais
-# Verificar se dois valores são iguais:
-# frozen_string_literal: true
+# Eq - usado para verificar se dois valores são iguais
+# Before Hook: Ele é útil para configurar um estado inicial compartilhado entre os testes.
 
-
-
-class Game
-  attr_accessor :title
-    def initialize (title)
-        @title = title
-    end
-end
+require "models/game"
 
 RSpec.describe 'Game' do
+  before(:each) do
+    @game = Game.new('Spider-Man: Miles Morales')
+  end
   context 'when dealing with PS5 games' do
-       it 'should have a title' do
-          game = Game.new('Spider-Man: Miles Morales')
-          expect(game.title).to eq('Spider-Man 2')                 
-       end 
-  end  
+    it 'should have a title' do
+      expect(@game.title).to eq('Spider-Man: Miles Morales')
+    end
+    it 'should allow setting and updating the title' do
+      @game.title = 'Demon Souls'
+      expect(@game.title).to eq('Demon Souls')
+      @game.title = 'Ratchet & Clank Rift Apart'
+      expect(@game.title).to eq('Ratchet & Clank Rift Apart')
+
+    end
+  end
 end
