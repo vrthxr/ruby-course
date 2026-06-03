8f78f60938b7e5a1608db8d870d1c5ad004cfb02
Arthur C <constancio_arthur@yahoo.com>
2026-06-03 14:04:05 -0300

    feat: add rspec setup with rails_helper and fix movie model and spec

--- aula-09-rspec/rspec-with-rails/teste_rails/app/models/movie.rb
+++ aula-09-rspec/rspec-with-rails/teste_rails/app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+    validates :title, presence: true
+    validates :duration, numericality: {greater_than: 0}
+
+    # método de calcular a média
+
+
+    def self.average_duration
+      total_duration = Movie.sum(:duration)
+      total_movies = Movie.count
+      return nil if total_movies.zero?
+      total_duration / total_movies
+    end
 end
--- aula-09-rspec/rspec-with-rails/teste_rails/spec/models/movie_spec.rb
+++ aula-09-rspec/rspec-with-rails/teste_rails/spec/models/movie_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    movie = Movie.new(
+      title: 'Example movie',
+      description: 'This is an example movie',
+      director: 'Jhon Doe',
+      duration: 120,
+    )
+
+    expect(movie).to be_valid
+  end
+
+  context "when  there are movies with duration" do
+    it "return the correct average duration" do
+      Movie.create(title:'Movie 1', duration: 120)
+      Movie.create(title:'Movie 2', duration: 90)
+      Movie.create(title:'Movie 2', duration: 150)
+
+      average_duration = Movie.average_duration
+
+      expect(average_duration).to eq(120)
+    end
+  end
 end
